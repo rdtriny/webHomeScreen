a7bfe0c33e3c70c8022806c54ebe239f5eb81b17
rdtriny <hbyangzhi0206@126.com>
2012-08-02 16:54:26 +0800

    add application widget management.

--- base.js
+++ base.js
@@ -144,6 +144,9 @@
 			}
 			var appNode = this.createAppNode(app);
 			this.display(appNode);
+			if(app.widget){
+				this.addWidget(app.widget ,appNode);
+			}
 		},
 		//change the location of apps
 		drag: function(){
@@ -224,8 +227,10 @@
 			}
 		},
 		//switch the pages left or right.
-		slideToPage: function(){
-		
+		slideToPage: function(pageIndex, time){
+			var pageWidth = document.getElementById("appScreen").clientWidth;
+			var x = pageIndex*pageWidth*-1;
+			this.css3move(this.container, {x:x, y:0}, time);
 		},
 		css3move: function(el, distance, time){
 			time = time || 0;
@@ -273,12 +278,133 @@
 		},
 		//notifacation releated issues.
 		notify: function(){
-		
+			
+		},
+		addWidget: function(widget, el){
+			var wdt = document.createElement("script");
+			wdt.src = widget;
+			wdt.type= "text/javascript";
+			document.head.appendChild(wdt);
+			window.el = el;
+			window.pageIndex = this.pagesCount;
+		},
+		removeWidget: function(widget){
+			var scripts = document.getElementsByTagName("script");
+			for(var i=0; i<scripts.length; i++){
+				if(scripts[i].src == widget){
+					scripts[i].parentNode.removeChild(scripts[i]);
+					return true;
+				}
+			}
+			return false;
 		}
 	};
 	return base;
 })(window);
 
+// sprite image Movie player 
+var spriteMovie = (function(){
+	function spriteMovie(imgSrc, flashWindowConf, movieArray){
+		this.imgSrc = imgSrc;
+		this.flashWindow = flashWindowConf;
+		this.movieArray = movieArray;
+		this.loadImage(imgSrc);
+	}
+	
+	spriteMovie.prototype = {
+		isImageLoad: false,
+		isbgset: false,
+		loadImage: function(imgSrc, callback){
+			var img = new Image();
+			img.src = imgSrc;
+			var that = this;
+			img.onload = function(){
+				console.log("image load completed!");
+				that.isImageLoad = true;
+				if(callback && typeof callback == "function"){
+					callback();
+				}
+				var notice = document.getElementById("notice");
+				if(notice){
+					notice.innerHTML = "Load completed!";
+				}
+			}
+		},
+		setFlashWindow: function(config, movieArray, startPos){
+			config.el.style.width = config.width+"px";
+			config.el.style.height = config.height+"px";
+			var that = this;
+			var bgInterval = setInterval(function(){
+				if(that.isImageLoad){
+					config.el.style.background = "url("+that.imgSrc+") top left no-repeat";
+					config.el.style.backgroundPosition = "-"+ movieArray[startPos].left +"px -"+ movieArray[startPos].top +"px";
+					that.isbgset = true;
+					if(that.wantPlayFps){
+						that.play(that.wantPlayFps);
+						that.wantPlayFps = 0;
+					}else if(that.wantRewindFps){
+						that.rewind(that.wantRewindFps);
+						that.wantRewindFps = 0;
+					}
+					clearInterval(bgInterval);
+				}
+			},10);
+		},
+		play: function(fps){					
+			this.setFlashWindow(this.flashWindow, this.movieArray, 0);
+			fps = fps || 16;
+			var time = 1000/fps;
+			this.wantPlayFps = fps;
+			if(this.isbgset){
+				this.wantPlayFps = 0;
+				var that = this;
+				this.frame = this.frame || 0;
+				clearInterval(this.start);
+				this.start = setInterval(function(){
+					that.flashWindow.el.style.backgroundPosition = "-"+that.movieArray[that.frame].left + "px -" +that.movieArray[that.frame].top+"px";
+					that.frame==(that.movieArray.length-1) ? (that.frame=0,that.stop()):(that.frame ++);
+				}, time);
+			}
+		},
+		stop: function(){
+			if(this.start){
+				clearInterval(this.start);
+				this.frame = 0;
+			}
+		},
+		pause: function(){
+			if(this.start){
+				clearInterval(this.start);
+			}
+		},
+		rewind: function(fps){
+			this.setFlashWindow(this.flashWindow, this.movieArray, this.movieArray.length-1);
+			fps = fps || 16;
+			var time = 1000/fps;
+			this.wantRewindFps = fps;
+			if(this.isbgset){
+				this.wantRewindFps = 0;
+				var that = this;
+				var frame = (this.movieArray.length-1);
+				clearInterval(this.start);
+				this.start = setInterval(function(){
+					that.flashWindow.el.style.backgroundPosition = "-"+that.movieArray[frame].left + "px -" +that.movieArray[frame].top+"px";
+					if(frame == 0){
+						if(that.callback){
+							console.log("call back");
+							that.callback();
+						}
+						clearInterval(that.start);								
+					}
+					else{
+						frame --;
+					}
+				}, time);
+			}
+		}
+	};
+	return spriteMovie;
+})();
 
 //for test.
 setTimeout(function(){
@@ -290,8 +416,8 @@ a.register({title:"facebook",packageName:"com.orange.twitter",imgSrc:"./ihome/tw
 a.register({title:"cloud",packageName:"com.orange.rss",imgSrc:"./ihome/rss.png",widget:""})
 a.register({title:"facebook",packageName:"com.orange.alarm",imgSrc:"./ihome/alarm.png",widget:""})
 a.register({title:"facebook",packageName:"com.orange.tv",imgSrc:"./ihome/tv.png",widget:""})
-a.register({title:"facebook",packageName:"com.orange.weather",imgSrc:"./ihome/weather.png",widget:""})
-a.register({title:"facebook",packageName:"com.orange.message",imgSrc:"./ihome/message.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.weather",imgSrc:"./ihome/message.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.message",imgSrc:"./ihome/weather.png",widget:"./widget/weather.js"})
 }, 2000);
 window.addEventListener("dbclick", function(e){
 	console.log(e.target.id+"         dbclick");
