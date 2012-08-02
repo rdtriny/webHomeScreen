4aa6804fc3c905b837eb2ed49154e7d5f7f78748
rdtriny <hbyangzhi0206@126.com>
2012-08-02 15:32:50 +0800

    Add a new feature: slide between pages, like native launcher.

--- base.js
+++ base.js
@@ -1,8 +1,7 @@
 ﻿var Base = (function(window, undefined){
-	
 	var base = function(container, config){
 		if(typeof container == "string"){
-			this.container = document.getElementById("iconsContainer");
+			this.container = document.getElementById(container);
 		}
 		else if(typeof container == "object" && container.nodeType == 1){
 			this.container = container;
@@ -30,7 +29,6 @@
 		}
 	};
 	base.prototype = {
-		appNum: 0,
 		appStyle: null,
 		startX: 0,
 		startY: 0,
@@ -39,6 +37,13 @@
 		longtapTime: 0,
 		dbclickInterval: null,
 		lastClickTime: 0,
+		appsPerRow: 4,
+		appsPerColumn: 1,
+		appsCount: 0,
+		pagesCount: 0,
+		currentPageIndex: 0,
+		movedDistance: 0,
+		moveStartX:0,
 		touchstart: function(e){
 			this.startX = e.touches[0].pageX;
 			this.startY = e.touches[0].pageY;
@@ -53,11 +58,14 @@
 			}, 1000);
 		},
 		touchmove: function(e){
-			if(this.longtapStart){
-				
-			}else{
-				clearTimeout(this.longTapInterval);
-				
+			if(!this.longtapStart){
+				clearTimeout(this.longTapInterval);	
+				var pagex = e.touches[0].pageX;
+				var pagey = e.touches[0].pageY;
+				var x = pagex-this.startX + this.moveStartX;
+				var dis = {x:x, y:0};
+				this.css3move(this.container, dis, 200);
+				this.movedDistance = pagex-this.startX;
 			}
 		},
 		touchend: function(e){
@@ -72,6 +80,32 @@
 				e.target.dispatchEvent(event);
 			}
 			this.lastClickTime = now;
+			
+			var pageWidth = document.getElementById("appScreen").clientWidth;
+			var percent = this.movedDistance / pageWidth;
+			console.log(percent);
+			
+			if(Math.abs(percent) > 0.06){
+				if(percent>0 && this.currentPageIndex>0){
+					var x = (this.currentPageIndex-1)*pageWidth*-1;
+					this.currentPageIndex -= 1;
+					console.log("right");
+				}
+				else if(percent<0 && this.currentPageIndex<this.pagesCount-1){
+					x = (this.currentPageIndex+1)*pageWidth*-1;
+					this.currentPageIndex += 1;
+					console.log("left");
+				}
+				else {
+					x = this.currentPageIndex*pageWidth*-1;
+					console.log("no move");
+				}
+			}
+			else {
+				x = this.currentPageIndex*pageWidth*-1;
+			}			
+			this.moveStartX = x;
+			this.css3move(this.container, {x:x, y:0}, 200);
 		},
 		click: function(e){
 			var target = e.target;
@@ -104,7 +138,10 @@
 		},
 		//register application to the system.
 		register: function(app){
-			this.appNum ++;
+			this.appsCount ++;
+			if(this.appsCount%(this.appsPerRow*this.appsPerColumn)==1){
+				this.managePages(1);
+			}
 			var appNode = this.createAppNode(app);
 			this.display(appNode);
 		},
@@ -132,7 +169,6 @@
 						var startColumn = Math.round(that.pageX/iconWidth);
 						target.style.left= (pagex-that.startX) + "px";
 						target.style.top = (pagey-that.startY) + "px";
-						console.log("yes");
 						if(row!=startRow || column!=startColumn){
 							row = row||1;
 							to = (row-1)*4+column;
@@ -146,12 +182,12 @@
 							icon[i].style.left="";
 							icon[i].style.top ="";
 						}
-						console.log("touch end");
 						that.container.ontouchmove = null;
 						that.container.ontouchend = null;
 						var tmpNode = target.cloneNode(true);
-						that.container.removeChild(target);
-						that.container.insertBefore(tmpNode, icon[to-1]);
+						console.log(document.getElementsByClassName("page").length);
+						document.getElementsByClassName("page")[that.currentPageIndex].removeChild(target);
+						document.getElementsByClassName("page")[that.currentPageIndex].insertBefore(tmpNode, icon[to-1]);
 					}
 				}
 			}, false);
@@ -167,7 +203,6 @@
 					from = i+1;
 				}
 			}
-			console.log(from + "        "+to);
 			if(from < to){
 				for(var i=from; i<to; i++){
 					if(i%4==1){
@@ -192,13 +227,40 @@
 		slideToPage: function(){
 		
 		},
-		//manage the pages which is hosting app icons.
-		managePages: function(){
-		
+		css3move: function(el, distance, time){
+			time = time || 0;
+			el.style.webkitTransform = 'translate3d('+ distance.x +'px, -' + distance.y + 'px,0)';
+			el.style.webkitTransitionDuration = time + 'ms';
+			el.style.webkitBackfaceVisiblity = 'hidden';
+			el.style.webkitTransformStyle = 'preserve-3d';
+			el.style.webkitTransitionTimingFunction = 'cubic-bezier(0.33,0.66,0.66,1)';
+		},
+		//manage the pages which is hosting app icons. inc for increment
+		managePages: function(inc){
+			var pages = document.getElementsByClassName("page");
+			this.pagesCount += inc;			
+			if(inc>0){
+				for(var i=0; i<inc; i++){
+					var page = document.createElement("div");
+					page.className = "page";
+					this.container.appendChild(page);
+				}
+			}
+			else{
+				for(var i=0; i<(-inc); i++){
+					this.container.removeChild(pages[this.pagesCount+i]);
+				}
+			}
+			
+			this.container.style.width = 100*this.pagesCount+"%";			
+			for(var j=0; j<this.pagesCount; j++){
+				pages[j].style.width = (100/this.pagesCount)+"%";
+			}
+			console.log("pageCount change to " + this.pagesCount);
 		},
 		//display all the app icons to the screen.
 		display: function(appNode){
-			this.container.appendChild(appNode);
+			document.getElementsByClassName("page")[this.pagesCount-1].appendChild(appNode);
 		},
 		//update the appearence of the app, change appStyle(a function)
 		updateAppStyle: function(func){
@@ -229,6 +291,7 @@ a.register({title:"cloud",packageName:"com.orange.rss",imgSrc:"./ihome/rss.png",
 a.register({title:"facebook",packageName:"com.orange.alarm",imgSrc:"./ihome/alarm.png",widget:""})
 a.register({title:"facebook",packageName:"com.orange.tv",imgSrc:"./ihome/tv.png",widget:""})
 a.register({title:"facebook",packageName:"com.orange.weather",imgSrc:"./ihome/weather.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.message",imgSrc:"./ihome/message.png",widget:""})
 }, 2000);
 window.addEventListener("dbclick", function(e){
 	console.log(e.target.id+"         dbclick");
