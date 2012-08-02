ed8a743598478e66dbd134b0a13c40fd166e6c53
rdtriny <hbyangzhi0206@126.com>
2012-08-02 14:06:10 +0800

    first commit

---
+++ base.js
@@ -0,0 +1,239 @@
+﻿var Base = (function(window, undefined){
+	
+	var base = function(container, config){
+		if(typeof container == "string"){
+			this.container = document.getElementById("iconsContainer");
+		}
+		else if(typeof container == "object" && container.nodeType == 1){
+			this.container = container;
+		}
+		if(config && typeof config == "object"){
+			for(var i in config){
+				this[i] = config[i];
+			}
+		}
+		var that = this;
+		this.container.addEventListener("touchstart", function(e){
+			that.touchstart(e);
+		}, false);
+		this.container.addEventListener("touchmove", function(e){
+			that.touchmove(e);
+		}, false);
+		this.container.addEventListener("touchend", function(e){
+			that.touchend(e);
+		}, false);
+		this.container.addEventListener("click", function(e){
+			that.click(e);
+		}, false);
+		if(this.dragable){
+			this.drag();
+		}
+	};
+	base.prototype = {
+		appNum: 0,
+		appStyle: null,
+		startX: 0,
+		startY: 0,
+		longTapInterval: null,
+		longtapStart: false,
+		longtapTime: 0,
+		dbclickInterval: null,
+		lastClickTime: 0,
+		touchstart: function(e){
+			this.startX = e.touches[0].pageX;
+			this.startY = e.touches[0].pageY;
+			
+			this.longtapTime = new Date;
+			var that = this;
+			this.longTapInterval = setTimeout(function(){
+				var event = document.createEvent("Events");
+				event.initEvent("longtap", true, true);
+				e.target.dispatchEvent(event);
+				that.longtapStart = true;
+			}, 1000);
+		},
+		touchmove: function(e){
+			if(this.longtapStart){
+				
+			}else{
+				clearTimeout(this.longTapInterval);
+				
+			}
+		},
+		touchend: function(e){
+			this.longtapStart = false;
+			if(this.longtapTime - new Date < 1000){
+				clearTimeout(this.longTapInterval);
+			}
+			var now = new Date;
+			if(now - this.lastClickTime<200){
+				var event = document.createEvent("Events");
+				event.initEvent("dbclick", true, true);
+				e.target.dispatchEvent(event);
+			}
+			this.lastClickTime = now;
+		},
+		click: function(e){
+			var target = e.target;
+			while(!target.id && target.id!="iconsContainer"){
+				target = target.parentNode;
+			}
+			if(/[A-z0-9]+\./ig.test(target.id)){
+				console.log(target.id);
+				//location.href = target.id;
+			}
+		},
+		createAppNode: function(app){
+			var appNode;
+			if(this.appStyle && typeof this.appStyle == "function"){
+				appNode = this.appStyle();
+			}else{
+				appNode = document.createElement("div");
+				appNode.className = "icon";
+				appNode.id = app.packageName;
+				
+				var img = document.createElement("img");
+				img.src = app.imgSrc;
+				appNode.appendChild(img);
+				
+				var span = document.createElement("span");
+				span.innerHTML = app.title;
+				appNode.appendChild(span);				
+			}
+			return appNode;
+		},
+		//register application to the system.
+		register: function(app){
+			this.appNum ++;
+			var appNode = this.createAppNode(app);
+			this.display(appNode);
+		},
+		//change the location of apps
+		drag: function(){
+			var that = this;
+			var to;
+			this.container.addEventListener("longtap", function(e){
+				var target = e.target;
+				while(!target.id && target.id!="iconsContainer"){
+					target = target.parentNode;
+				}
+				if(/[A-z0-9]+\./ig.test(target.id)){
+					target.style.webkitTransform = "scale(1.3)";
+					that.container.ontouchmove = function(e){
+						var pagex = e.touches[0].pageX;
+						var pagey = e.touches[0].pageY;
+						var icon = document.getElementsByClassName("icon")[0];
+						var iconHeight = icon.clientHeight;
+						var iconWidth = icon.clientWidth;
+						
+						var row = Math.round(pagey/iconHeight); //20% height
+						var column = Math.round(pagex/iconWidth); //25% width
+						var startRow = Math.round(that.pageY/iconHeight);
+						var startColumn = Math.round(that.pageX/iconWidth);
+						target.style.left= (pagex-that.startX) + "px";
+						target.style.top = (pagey-that.startY) + "px";
+						console.log("yes");
+						if(row!=startRow || column!=startColumn){
+							row = row||1;
+							to = (row-1)*4+column;
+							that.switchNode(target, (row-1)*4+column);
+						}
+					};
+					that.container.ontouchend = function(e){
+						var icon = document.getElementsByClassName("icon");
+						target.style.webkitTransform = "";
+						for(var i=0; i<icon.length; i++){
+							icon[i].style.left="";
+							icon[i].style.top ="";
+						}
+						console.log("touch end");
+						that.container.ontouchmove = null;
+						that.container.ontouchend = null;
+						var tmpNode = target.cloneNode(true);
+						that.container.removeChild(target);
+						that.container.insertBefore(tmpNode, icon[to-1]);
+					}
+				}
+			}, false);
+		},
+		switchNode: function(node, to){
+			var icon = document.getElementsByClassName("icon");
+			var from;
+			var len = icon.length;
+			if(to>len)
+				to = len;
+			for(var i=0; i<len; i++){
+				if(node.id == icon[i].id){
+					from = i+1;
+				}
+			}
+			console.log(from + "        "+to);
+			if(from < to){
+				for(var i=from; i<to; i++){
+					if(i%4==1){
+						icon[i].style.left="75%";
+						icon[i].style.top ="-20%";
+					}else{
+						icon[i].style.left="-25%";
+					}
+				}
+			}else{
+				for(var i=to-1; i<from-1; i++){
+					if(i%4==0){
+						icon[i].style.left="-75%";
+						icon[i].style.top ="20%";
+					}else{
+						icon[i].style.left="25%";
+					}
+				}
+			}
+		},
+		//switch the pages left or right.
+		slideToPage: function(){
+		
+		},
+		//manage the pages which is hosting app icons.
+		managePages: function(){
+		
+		},
+		//display all the app icons to the screen.
+		display: function(appNode){
+			this.container.appendChild(appNode);
+		},
+		//update the appearence of the app, change appStyle(a function)
+		updateAppStyle: function(func){
+			if(func && typeof func =="function"){
+				this.appStyle = func;
+				return true;
+			}else{
+				return false;
+			}
+		},
+		//notifacation releated issues.
+		notify: function(){
+		
+		}
+	};
+	return base;
+})(window);
+
+
+//for test.
+setTimeout(function(){
+var a = new Base("iconsContainer",{dragable:true});
+a.register({title:"cloud",packageName:"com.orange.cloud",imgSrc:"./ihome/music.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.facebook",imgSrc:"./ihome/facebook.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.movie",imgSrc:"./ihome/movie.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.twitter",imgSrc:"./ihome/twitter.png",widget:""})
+a.register({title:"cloud",packageName:"com.orange.rss",imgSrc:"./ihome/rss.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.alarm",imgSrc:"./ihome/alarm.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.tv",imgSrc:"./ihome/tv.png",widget:""})
+a.register({title:"facebook",packageName:"com.orange.weather",imgSrc:"./ihome/weather.png",widget:""})
+}, 2000);
+window.addEventListener("dbclick", function(e){
+	console.log(e.target.id+"         dbclick");
+}, false);
+window.addEventListener("longtap", function(e){
+	console.log(e.target.id+"         logntap");
+	e.preventDefault();
+}, false);
