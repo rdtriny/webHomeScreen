e82c58657cf93a0c00c5a7c89bbd4a5cff47c45b
杨志 <hbyangzhi0206@126.com>
2012-09-21 17:43:50 +0800

    Add some explanation in _Base_.js .

--- sys/v2.2.1 beta/_Base_.js
+++ sys/v2.2.1 beta/_Base_.js
@@ -3,14 +3,21 @@
 	var base = function(container, config){
 	
 		//copy the configs into the base's prototype.
+		
 		if(config && typeof config == "object"){
 			for(var i in config){
 				// Don't use base.config = config, because it may cover other properties.
+				
 				base.Config[i] = config[i];
 			}
 		}
 		
 	};
+	
+	/*
+	 |	Default configuration of the system, _Base_ will apply these configurations if the user doesn't speicify 
+	 |	these configurations.
+	*/
 	
 	base.Config = {
 		isVertical: true,
@@ -20,17 +27,17 @@
 		appsPerColumn: 4
 	};
 	
-	//
-	//	a interface which to include properties from other objects.
-	//	example:
-	//		var target = {};
-	//		base.extend(target,{a:1,b:2},{c:[1,2,3]});
-	//	parameters:
-	//		target, the target object which wants to extend more properties.
-	//		(optinal)obj, the object to give target properties.
-	//		 .....
-	//		(optinal)objN, the nth object to give target properties.
-	//
+	/*
+	|	a interface which to include properties from other objects.
+	|	example:
+	|		var target = {};
+	|		base.extend(target,{a:1,b:2},{c:[1,2,3]});
+	|	parameters:
+	|		target, the target object which wants to extend more properties.
+	|		(optinal)obj, the object to give target properties.
+	|		 .....
+	|		(optinal)objN, the nth object to give target properties.
+	*/
 	base.extend = function(target){
 		var target = arguments[0];
 		if(typeof target != "object" && typeof target != "function"){
@@ -47,6 +54,16 @@
 		return target;
 	};
 	
+	/*
+	 | With require Method, we can include specific javascript file at runtime. 
+	 | Usage:
+	 |		1. use an array, files in this array will be loaded one by one.
+	 |			['a.js', 'b.js', 'c.js'].require();
+	 |
+	 |		2. Just want to include one file.
+	 |			['a.js'].require(); Or _Base_.require('a.js');
+	 |
+	*/
 	Array.prototype.require = base.require = function(file){
 		var type = toString.call(this),
 			target = [];
@@ -72,7 +89,11 @@
 
 	window._Base_ = base;
 	
-		
+	/*
+	 | load all the components one by one.
+	 |		the order of the order of the array donesn't matter.
+	 |		some Key Functions, including: Browser, System, Drive, Page, App, Queue, Widget;
+	*/
 	['./js/Ajax', './js/Debug', './js/DOM', './js/Browser', './js/System', './js/Drive', './js/Page', './js/App', './js/Sidebar', './js/Box', './js/Queue', './js/Widget'
 	,'./js/Tray', './js/Notify', './js/Helper', './js/Sound', './js/spriteMovie'].require();
 	
