132c683d27ab4485ba644b64f687152539c7bb64
杨志 <hbyangzhi0206@126.com>
2012-10-08 15:04:58 +0800

    a small modification.

--- sys/v2.2.1 beta/Helper.js
+++ sys/v2.2.1 beta/Helper.js
@@ -7,6 +7,14 @@
 				result = 0;
 			}
 			return result;
+		},
+		randomColor: function(){
+			var colors = [{deep: "#69c278", light: "#c4e7ca"},
+						  {deep: "#c2b469", light: "#e7e2c4"},
+						  {deep: "#c26969", light: "#e7c4c4"},
+						  {deep: "#7869c2", light: "#cbc4e7"}];
+			var result = colors[Math.floor(100*Math.random())%4];
+			return result;
 		}
 	});
 	
